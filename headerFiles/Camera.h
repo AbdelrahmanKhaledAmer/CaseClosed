da9a76aa228ff52434fd5056e3b438205dddbd0e
AbdelrahmanKhaledAmer <abdel-rahman.soliman@student.guc.edu.eg>
2017-11-30 01:53:59 +0200

    Added Eigen library for vector and matrix operations, and finished initial camera class. Cleanup will be done in the next commit.

--- headerFiles/Camera.h
+++ headerFiles/Camera.h
@@ -1,10 +1,24 @@
-#include <glut.h>
+#include <Eigen/Dense>
 
 class Camera
 {
 private:
-
+    Eigen::Vector3f eye;
+    Eigen::Vector3f lookAt;
+    Eigen::Vector3f orientation;
+    float translationStep = 0.1;
+    float rotationAngle = 0.0174532925199433;
 public:
+    // Constructor for camera object
+    Camera(Eigen::Vector3f eye, Eigen::Vector3f lookAt, Eigen::Vector3f orientation)
+    {
+        this->eye = eye;
+        this->lookAt = lookAt;
+        this->orientation = orientation;
+    }
+    // Destructor for camera object
+    ~Camera() { }
+    
     // Function to set up all camera variables
     void setup()
     {
@@ -14,15 +28,179 @@ public:
 
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
-        cameraPosition();
-        //gluLookAt(15, 15, 15, 0.0, 10.0, 0.0, 0.0, 1.0, 0.0);
+        gluLookAt(eye.x(), eye.y(), eye.z(), lookAt.x(), lookAt.y(), lookAt.z(), orientation.x(), orientation.y(), orientation.z());
     }
 
-    // Constructor for camera object
-    Camera() { }
-    // Destructor for camera object
-    ~Camera() { }
-    
+    // Rotation Functions
+    void rotateRight(float scale = 1)
+    {
+        float theta = rotationAngle * scale / 2;
+        Eigen::Quaternionf rotAxis(theta, orientation.x(), orientation.y(), orientation.z());
+        rotAxis.normalize();
+        Eigen::Quaternionf VectorToRotate;
+        VectorToRotate.w() = 0;
+        VectorToRotate.vec() = eye - lookAt;
+        lookAt = eye + (rotAxis * VectorToRotate * rotAxis.inverse()).vec();
+        Eigen::Quaternionf rotAxis2(theta, orientation.x(), orientation.y(), orientation.z());
+        rotAxis2.normalize();
+        Eigen::Quaternionf VectorToRotate2;
+        VectorToRotate2.w() = 0;
+        VectorToRotate2.vec() = eye - lookAt;
+        lookAt = eye + (rotAxis2 * VectorToRotate2 * rotAxis2.inverse()).vec();
+    }
+
+    void rotateLeft(float scale = 1)
+    {
+        float theta = - rotationAngle * scale / 2;
+        Eigen::Quaternionf rotAxis(theta, orientation.x(), orientation.y(), orientation.z());
+        rotAxis.normalize();
+        Eigen::Quaternionf VectorToRotate;
+        VectorToRotate.w() = 0;
+        VectorToRotate.vec() = eye - lookAt;
+        lookAt = eye + (rotAxis * VectorToRotate * rotAxis.inverse()).vec();
+        Eigen::Quaternionf rotAxis2(theta, orientation.x(), orientation.y(), orientation.z());
+        rotAxis2.normalize();
+        Eigen::Quaternionf VectorToRotate2;
+        VectorToRotate2.w() = 0;
+        VectorToRotate2.vec() = eye - lookAt;
+        lookAt = eye + (rotAxis2 * VectorToRotate2 * rotAxis2.inverse()).vec();
+    }
+
+    void rotateUp(float scale = 1)
+    {
+        float theta = -rotationAngle * scale / 2;
+        Eigen::Vector3f newX = orientation.cross(eye - lookAt);
+        Eigen::Quaternionf rotAxis(theta, newX.x(), newX.y(), newX.z());
+        rotAxis.normalize();
+        Eigen::Quaternionf VectorToRotate;
+        VectorToRotate.w() = 0;
+        VectorToRotate.vec() = eye - lookAt;
+        lookAt = eye + (rotAxis * VectorToRotate * rotAxis.inverse()).vec();
+        Eigen::Quaternionf rotAxis2(theta, newX.x(), newX.y(), newX.z());
+        rotAxis2.normalize();
+        Eigen::Quaternionf VectorToRotate2;
+        VectorToRotate2.w() = 0;
+        VectorToRotate2.vec() = eye - lookAt;
+        lookAt = eye + (rotAxis2 * VectorToRotate2 * rotAxis2.inverse()).vec();
+    }
+
+    void rotateDown(float scale = 1)
+    {
+        float theta = rotationAngle * scale / 2;
+        Eigen::Vector3f newX = orientation.cross(eye - lookAt);
+        Eigen::Quaternionf rotAxis(theta, newX.x(), newX.y(), newX.z());
+        rotAxis.normalize();
+        Eigen::Quaternionf VectorToRotate;
+        VectorToRotate.w() = 0;
+        VectorToRotate.vec() = eye - lookAt;
+        lookAt = eye + (rotAxis * VectorToRotate * rotAxis.inverse()).vec();
+        Eigen::Quaternionf rotAxis2(theta, newX.x(), newX.y(), newX.z());
+        rotAxis2.normalize();
+        Eigen::Quaternionf VectorToRotate2;
+        VectorToRotate2.w() = 0;
+        VectorToRotate2.vec() = eye - lookAt;
+        lookAt = eye + (rotAxis2 * VectorToRotate2 * rotAxis2.inverse()).vec();
+    }
+
+    void tiltRight(float scale = 1)
+    {
+        float theta = rotationAngle * scale / 2;
+        Eigen::Vector3f newZ = eye - lookAt;
+        Eigen::Quaternionf rotAxis(theta, newZ.x(), newZ.y(), newZ.z());
+        rotAxis.normalize();
+        Eigen::Quaternionf VectorToRotate;
+        VectorToRotate.w() = 0;
+        VectorToRotate.vec() = orientation;
+        orientation = (rotAxis * VectorToRotate * rotAxis.inverse()).vec();
+        Eigen::Quaternionf rotAxis2(theta, newZ.x(), newZ.y(), newZ.z());
+        rotAxis2.normalize();
+        Eigen::Quaternionf VectorToRotate2;
+        VectorToRotate2.w() = 0;
+        VectorToRotate2.vec() = orientation;
+        orientation = (rotAxis2 * VectorToRotate2 * rotAxis2.inverse()).vec();
+    }
+
+    void tiltLeft(float scale = 1)
+    {
+        float theta = -rotationAngle * scale / 2;
+        Eigen::Vector3f newZ = eye - lookAt;
+        Eigen::Quaternionf rotAxis(theta, newZ.x(), newZ.y(), newZ.z());
+        rotAxis.normalize();
+        Eigen::Quaternionf VectorToRotate;
+        VectorToRotate.w() = 0;
+        VectorToRotate.vec() = orientation;
+        orientation = (rotAxis * VectorToRotate * rotAxis.inverse()).vec();
+        Eigen::Quaternionf rotAxis2(theta, newZ.x(), newZ.y(), newZ.z());
+        rotAxis2.normalize();
+        Eigen::Quaternionf VectorToRotate2;
+        VectorToRotate2.w() = 0;
+        VectorToRotate2.vec() = orientation;
+        orientation = (rotAxis2 * VectorToRotate2 * rotAxis2.inverse()).vec();
+    }
+
+    // Translation Functions
+    void translateForward(float scale = 1)
+    {
+        float step = translationStep * scale;
+        Eigen::Vector3f direction = (lookAt - eye).normalized();
+        direction *= step;
+        direction.y() = 0;
+        eye += direction;
+        lookAt += direction;
+    }
+
+    void translateBackward(float scale = 1)
+    {
+        float step = translationStep * scale;
+        Eigen::Vector3f direction = (lookAt - eye).normalized();
+        direction *= step;
+        direction.y() = 0;
+        eye -= direction;
+        lookAt -= direction;
+    }
+
+    void translateLeft(float scale = 1)
+    {
+        float step = translationStep * scale;
+        Eigen::Vector3f direction = (orientation.cross(lookAt - eye)).normalized();
+        direction *= step;
+        direction.y() = 0;
+        eye += direction;
+        lookAt += direction;
+    }
+
+    void translateRight(float scale = 1)
+    {
+        float step = translationStep * scale;
+        Eigen::Vector3f direction = (orientation.cross(lookAt - eye)).normalized();
+        direction *= step;
+        direction.y() = 0;
+        eye -= direction;
+        lookAt -= direction;
+    }
+
+    void translateUp(float scale = 1)
+    {
+        float step = translationStep * scale;
+        Eigen::Vector3f direction = (orientation).normalized();
+        direction *= step;
+        direction.x() = 0;
+        direction.z() = 0;
+        eye += direction;
+        lookAt += direction;
+    }
+
+    void translateDown(float scale = 1)
+    {
+        float step = translationStep * scale;
+        Eigen::Vector3f direction = (orientation).normalized();
+        direction *= step;
+        direction.x() = 0;
+        direction.z() = 0;
+        eye -= direction;
+        lookAt -= direction;
+    }
+
     // Reset all variables to base values if needed
     void reset()
     {
--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 #include "headerFiles/Model_3DS.h"
 #include "headerFiles/GLTexture.h"
 #include <math.h>
-#include <windows.h>
+#include <iostream>
+#include <Eigen/Dense>
 #include <glut.h>
 #include "headerFiles/Axes.h"
 #include "headerFiles/Camera.h"
@@ -16,17 +17,24 @@ const int scale = 70;
 const int width = 16 * scale;
 const int height = 9 * scale;
 
+Eigen::Vector3f eye(1,0.2,1);
+Eigen::Vector3f lookAt(0,0,0);
+Eigen::Vector3f orientation(0,1,0);
+
+Camera camera(eye, lookAt, orientation);
+
 void display(void)
 {
-
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	// Setup light
 	Lights::initLightSource();
 	Lights::setupLights();
 	// Set the camera
+	camera.setup();
 
-	//
+	// Axes for modeling 
 	Axes axes(0.5);
+
 	// Reset color and flush buffer
 	glColor3f(1.0, 1.0, 1.0);
 	glFlush();
@@ -40,6 +48,50 @@ void LoadAssets()
 	// Starting music
 }
 
+void key(unsigned char k, int x, int y)
+{
+	switch(k)
+	{
+	case 'l':
+		camera.rotateRight();
+		break;
+	case 'j':
+		camera.rotateLeft();
+		break;
+	case 'i':
+		camera.rotateUp();
+		break;
+	case 'k':
+		camera.rotateDown();
+		break;
+	case 'o':
+		camera.tiltRight();
+		break;
+	case 'u':
+		camera.tiltLeft();
+		break;
+	case 'w':
+		camera.translateForward();
+		break;
+	case 's':
+		camera.translateBackward();
+		break;
+	case 'a':
+		camera.translateLeft();
+		break;
+	case 'd':
+		camera.translateRight();
+		break;
+	case 'e':
+		camera.translateUp();
+		break;
+	case 'q':
+		camera.translateDown();
+		break;
+	}
+	glutPostRedisplay();
+}
+
 void main(int argc, char** argv)
 {
 	glutInit(&argc, argv);
@@ -50,6 +102,7 @@ void main(int argc, char** argv)
 	glutCreateWindow("Game Title");
 
 	glutDisplayFunc(display);
+	glutKeyboardFunc(key);
 
 	glClearColor(1, 1, 1, 0);
 
