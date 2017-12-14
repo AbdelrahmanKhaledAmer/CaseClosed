698b0a43399ac4c317109b988b7f4cd0ae8a7208
markrofail <markm.rofail@gmail.com>
2017-12-14 02:37:44 +0200

    feat: create all journall clues+variantes

--- sourceFiles/Objects/Journal.cpp
+++ sourceFiles/Objects/Journal.cpp
@@ -4,7 +4,8 @@
 Constructor for the Journal object.
 Returns a pointer for a Journal.
 
-@param (location) The current location of the Journal with respect to the global origin.
+@param (location) The current location of the Journal with respect to the global
+origin.
 @param (orientation) The current rotation angles across all three axes.
 @param (scale) The amount to scale across all three axes.
 @param (dimensions) The length of the object along every axis before scaling.
@@ -27,10 +28,10 @@ void Journal::write(int clueIndex, int property) {
 }
 
 void Journal::draw() {
-	float minX = location_.x();
-	float maxX = location_.x() + dimensions_.x();
-	float minY = location_.y();
-	float maxY = location_.y()+ dimensions_.y();
+  float minX = location_.x();
+  float maxX = location_.x() + dimensions_.x();
+  float minY = location_.y();
+  float maxY = location_.y() + dimensions_.y();
 
   glPushMatrix();
   {
@@ -40,7 +41,7 @@ void Journal::draw() {
     drawImage(minX, maxX, minY, maxY, journalImage_);
     for (int i = 0; i < 10; i++) {
       if (clues_[i] == true) {
-        glTranslatef(0, 0, (i+1) * 0.01);
+        glTranslatef(0, 0, (i + 1) * 0.01);
 
         switch (cluesState_[i]) {
         case NOT_STATED:
@@ -53,6 +54,7 @@ void Journal::draw() {
           drawImage(minX, maxX, minY, maxY, clueImages_[i][2]);
           break;
         default:
+          break;
         }
       }
     }
@@ -60,9 +62,38 @@ void Journal::draw() {
   glPopMatrix();
 }
 
-void Journal::setModel(){
-	journalImage_ = loadImage("assets/images/Journal/journal.png");
-	clueImages_[0][0] = loadImage("assets/images/Journal/clue_0.png"); 
-	clueImages_[0][1] = loadImage("assets/images/Journal/clueRelevant_0.png"); 
-	clueImages_[0][2] = loadImage("assets/images/Journal/clueIrrelevant_0.png"); 
+void Journal::setModel() {
+  journalImage_ = loadImage("assets/images/Journal/journal.png");
+
+  clueImages_[0][0] = loadImage("assets/images/Journal/clue_0.png");
+  clueImages_[0][1] = loadImage("assets/images/Journal/clue_0_relevant.png");
+  clueImages_[0][2] = loadImage("assets/images/Journal/clue_0_irrelevant.png");
+
+  clueImages_[1][0] = loadImage("assets/images/Journal/clue_1.png");
+  clueImages_[1][1] = loadImage("assets/images/Journal/clue_1_relevant.png");
+  clueImages_[1][2] = loadImage("assets/images/Journal/clue_1_irrelevant.png");
+
+  clueImages_[2][0] = loadImage("assets/images/Journal/clue_2.png");
+  clueImages_[2][1] = loadImage("assets/images/Journal/clue_2_relevant.png");
+  clueImages_[2][2] = loadImage("assets/images/Journal/clue_2_irrelevant.png");
+
+  clueImages_[3][0] = loadImage("assets/images/Journal/clue_3.png");
+  clueImages_[3][1] = loadImage("assets/images/Journal/clue_3_relevant.png");
+  clueImages_[3][2] = loadImage("assets/images/Journal/clue_3_irrelevant.png");
+
+  clueImages_[4][0] = loadImage("assets/images/Journal/clue_4.png");
+  clueImages_[4][1] = loadImage("assets/images/Journal/clue_4_relevant.png");
+  clueImages_[4][2] = loadImage("assets/images/Journal/clue_4_irrelevant.png");
+
+  clueImages_[5][0] = loadImage("assets/images/Journal/clue_5.png");
+  clueImages_[5][1] = loadImage("assets/images/Journal/clue_5_relevant.png");
+  clueImages_[5][2] = loadImage("assets/images/Journal/clue_5_irrelevant.png");
+
+  clueImages_[6][0] = loadImage("assets/images/Journal/clue_6.png");
+  clueImages_[6][1] = loadImage("assets/images/Journal/clue_6_relevant.png");
+  clueImages_[6][2] = loadImage("assets/images/Journal/clue_6_irrelevant.png");
+
+  clueImages_[7][0] = loadImage("assets/images/Journal/clue_7.png");
+  clueImages_[7][1] = loadImage("assets/images/Journal/clue_7_relevant.png");
+  clueImages_[7][2] = loadImage("assets/images/Journal/clue_7_irrelevant.png");
 }
